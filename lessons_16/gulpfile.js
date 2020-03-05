6a430146674967520126ad3e6955b227a3ccad48
Neksov <n2v@list.ru>
2020-03-05 17:03:39 +0300

    rewrite #15

--- lessons_16/gulpfile.js
+++ lessons_16/gulpfile.js
@@ -1,12 +1,9 @@
-const {
-  src,
-  dest,
-  watch
-} = require("gulp");
+const { src, dest, watch } = require("gulp");
 const browserSync = require("browser-sync").create();
 const cssmin = require("gulp-cssmin");
 const rename = require("gulp-rename");
-const sass = require("gulp-sass")
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
 // Static server, обновление старницы автоматом
 function bs() {
@@ -19,10 +16,8 @@ function bs() {
   });
   watch("./*.html").on("change", browserSync.reload);
   watch("./sass/**/*.sass", serverSass);
-  watch("./scss/**/*.scss", serverSass);
   watch("./js/*.js").on("change", browserSync.reload);
-
-};
+}
 
 //CSSMIN ДОРАБОТАТЬ
 //function cssmin() {
@@ -36,14 +31,17 @@ function bs() {
 //  .pipe(dest("./css"));
 //};
 
-
-
 //SASS
 function serverSass() {
-  return src("./sass/**/*.sass", "./scss/**/*.scss")
+  return src("./sass/*.sass")
     .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
     .pipe(dest("./css"))
     .pipe(browserSync.stream());
-};
+}
 
 exports.server = bs;
