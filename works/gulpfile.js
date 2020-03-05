9bee0342a9c74ffa97ec60dadbbbef118b188836
Neksov <n2v@list.ru>
2020-03-05 16:52:34 +0300

    add #16

--- works/gulpfile.js
+++ works/gulpfile.js
@@ -1,12 +1,9 @@
-const {
-  src,
-  dest,
-  watch
-} = require("gulp");
+const { src, dest, watch } = require("gulp");
 const browserSync = require("browser-sync").create();
 const cssmin = require("gulp-cssmin");
 const rename = require("gulp-rename");
-const sass = require("gulp-sass")
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
 // Static server, обновление старницы автоматом
 function bs() {
@@ -20,8 +17,7 @@ function bs() {
   watch("./*.html").on("change", browserSync.reload);
   watch("./sass/**/*.sass", serverSass);
   watch("./js/*.js").on("change", browserSync.reload);
-
-};
+}
 
 //CSSMIN ДОРАБОТАТЬ
 //function cssmin() {
@@ -35,14 +31,17 @@ function bs() {
 //  .pipe(dest("./css"));
 //};
 
-
-
 //SASS
 function serverSass() {
   return src("./sass/*.sass")
     .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
     .pipe(dest("./css"))
     .pipe(browserSync.stream());
-};
+}
 
 exports.server = bs;
