ebbdd64df1d37a5704b09a07826d44e1b4da9427
Neksov <n2v@list.ru>
2020-03-13 22:04:33 +0300

    feat: add close esc

--- lessons_20/js/main.js
+++ lessons_20/js/main.js
@@ -1,26 +1,27 @@
-/*document.addEventListener("DOMContetntLoaded", function (event) {
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle = modal]');
-  const closeBtn = document.querySelector('modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
-  modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
-  });
-  closeBtn.addEventListener('click', switchModal);
-});*/
-
 document.addEventListener("DOMContentLoaded", function (event) {
   //do work
-  const modal = document.querySelector('.modal');
-  const modalBtn = document.querySelectorAll('[data-toggle = modal]');
-  const closeBtn = document.querySelector('.modal__close');
-  const switchModal = () => {
-    modal.classList.toggle('modal--visible');
-  }
+  let modal = document.querySelector(".modal");
+  let modalBtn = document.querySelectorAll("[data-toggle = modal]");
+  let closeBtn = document.querySelector(".modal__close");
+  let switchModal = () => {
+    modal.classList.toggle("modal--visible");
+  };
   modalBtn.forEach(element => {
-    element.addEventListener('click', switchModal);
+    element.addEventListener("click", switchModal);
   });
-  closeBtn.addEventListener('click', switchModal);
+
+  closeBtn.addEventListener("click", switchModal);
+
+  document.addEventListener("click", e => {
+    if (e.target == modal) {
+      modal.classList.remove("modal--visible");
+    }
+  })
+
+  document.addEventListener('keydown', function (e) {
+    switch (e.keyCode) {
+      case 27:
+        switchModal();
+    }
+  });
 });
