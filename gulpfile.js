352be2b32cb415024f6fcc13adb53d78a26b51d5
Neksov <n2v@list.ru>
2020-03-17 15:10:03 +0300

    new

--- gulpfile.js
+++ gulpfile.js
@@ -6,7 +6,8 @@ const {
 const browserSync = require("browser-sync").create();
 const cssmin = require("gulp-cssmin");
 const rename = require("gulp-rename");
-const sass = require("gulp-sass")
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
 
 // Static server, обновление старницы автоматом
 function bs() {
@@ -20,8 +21,7 @@ function bs() {
   watch("./*.html").on("change", browserSync.reload);
   watch("./sass/**/*.sass", serverSass);
   watch("./js/*.js").on("change", browserSync.reload);
-
-};
+}
 
 //CSSMIN ДОРАБОТАТЬ
 //function cssmin() {
@@ -35,17 +35,17 @@ function bs() {
 //  .pipe(dest("./css"));
 //};
 
-
-
 //SASS
 function serverSass() {
   return src("./sass/*.sass")
     .pipe(sass())
-    .pipe(autoprefixer({
-      cascade: false
-    }))
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
     .pipe(dest("./css"))
     .pipe(browserSync.stream());
-};
+}
 
 exports.server = bs;
