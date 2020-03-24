e66b7ce67bba601e2c13352175f1dcf515233598
Neksov <n2v@list.ru>
2020-03-25 00:15:07 +0300

    add img

--- lessons_28/gulpfile.js
+++ lessons_28/gulpfile.js
@@ -50,7 +50,7 @@ exports.server = bs;
 
 gulp.task("compress", function () {
   gulp
-    .src("img/*") //папка из которой берем
+    .src("img/types/*") //папка из которой берем
     .pipe(imagemin())
     .pipe(gulp.dest("img/images/")); // папка в которую кладем
 });
